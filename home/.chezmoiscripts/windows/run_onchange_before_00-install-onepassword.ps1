0ae302cfcc0aa85d2fbe0b95658eb9334f22acdb
Andrew Burgess <andrew@andrewburgess.io>
2025-01-12 12:48:30 -0800

    setup windows package installation

---
+++ home/.chezmoiscripts/windows/run_onchange_before_00-install-onepassword.ps1
@@ -0,0 +1,12 @@
+# Ensure winget is available
+if (-not (Get-Command winget -ErrorAction SilentlyContinue)) {
+    Write-Host "winget is not available. Quitting..."
+    exit 1
+}
+
+# Install 1Password
+winget install AgileBits.1Password -e
+winget install AgileBits.1Password.CLI -e
+
+# Prompt user to login to 1Password and enable CLI
+$Answer = Read-Host "Please log into 1Password and enable the CLI. Press 'y' when done."
