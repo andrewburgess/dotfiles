90d0627819c32d63b02059e3577742c869be47f8
Andrew Burgess <andrewb@withhugo.com>
2026-03-28 14:38:23 -0700

    Update .chezmoiscripts/macos/install-firacode-nerd-font.sh Update .zshrc

--- home/.chezmoiscripts/macos/run_once_install-firacode-nerd-font.sh
+++ home/.chezmoiscripts/macos/run_once_install-firacode-nerd-font.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-brew list --cask font-fira-code-nerd-font &>/dev/null || brew install --cask font-fira-code-nerd-font
+brew list --cask font-fira-code-nerd-font &>/dev/null || brew install --cask --force font-fira-code-nerd-font
